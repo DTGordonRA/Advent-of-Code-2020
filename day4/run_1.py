2bd1be10fbabffc02c322ff74b8680d52e8c0a02
David Gordon <david.gordon@rookadvisors.com>
2020-12-04 13:32:07 -0700

    Removed extra code, commented out prints

--- day4/run_1.py
+++ day4/run_1.py
@@ -7,10 +7,6 @@ required_fields = [
     "ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"
 ]
 
-hcl_codes = {'0':1, '1':1, '2':1, '3':1, '4':1, '5':1, '6':1, '7':1, '8':1, '9':1, 'a':1, 'b':1, 'c':1, 'd':1, 'e':1, 'f':1}
-ecl_codes = {"amb":1,"blu":1,"brn":1,"gry":1,"grn":1,"hzl":1,"oth":1}
-pid_codes = {'0':1, '1':1, '2':1, '3':1, '4':1, '5':1, '6':1, '7':1, '8':1, '9':1}
-
 loop = True
 line = passports.readline().strip()
 
@@ -36,7 +32,7 @@ while(loop):
     if(valid == True):
         passport_count += 1
     
-    print(parts)
+    #print(parts)
 
     line = passports.readline().strip()
     if(line == ''):
